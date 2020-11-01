5567502d1741e89a6fde4339f7735fa2cd5257a4
hiba zalloum <hebahzalloum@gmail.com>
2020-11-01 13:47:30 +0200

    3th

--- Home work 3.py
+++ Home work 3.py
@@ -15,6 +15,20 @@ class Matrix:
                for i in range(3)]
         return Matrix(Sum)
 
+    def sub(self, other):
+        mat1 = self.matrix
+        mat2 = other.matrix
+        Sub = [[mat1[i][j] - mat1[i][j] for j in range(3)]
+               for i in range(3)]
+        return Matrix(Sub)
+
+    def multi(self, other):
+        mat1 = self.matrix
+        mat2 = other.matrix
+        mul = [[mat1[i][j] * mat1[i][j] for j in range(3)]
+               for i in range(3)]
+        return Matrix(mul)
+
 
 m1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 # print(m1.matrixForm())
